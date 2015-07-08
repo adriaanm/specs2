4313df9f77494bc7987576712d6113fb98c095a7
Adriaan Moors <adriaan.moors@typesafe.com>
2015-07-08 16:45:14 -0700

    Treat 2.12 as 2.11 for source version

--- project/build.scala
+++ project/build.scala
@@ -171,10 +171,12 @@ object build extends Build {
     (if (name == "specs2") "" else name) + "> " 
   }
 
+  def scalaSourceVersion(scalaBinaryVersion: String) = if (scalaBinaryVersion.startsWith("2.10")) "2.10" else "2.11"
+
   lazy val compilationSettings: Seq[Settings] = Seq(
     // https://gist.github.com/djspiewak/976cd8ac65e20e136f05
     unmanagedSourceDirectories in Compile ++=
-      Seq((sourceDirectory in Compile).value / s"scala-${scalaBinaryVersion.value}",
+      Seq((sourceDirectory in Compile).value / s"scala-${scalaSourceVersion(scalaBinaryVersion.value)}",
           (if (scalazVersion.value.startsWith("7.0")) (sourceDirectory in Compile).value / s"scala-scalaz-7.0.x"
            else                                       (sourceDirectory in Compile).value / s"scala-scalaz-7.1.x"),
           (if (scalazVersion.value.startsWith("7.0")) (sourceDirectory in (Test, test)).value / s"scala-scalaz-7.0.x"
